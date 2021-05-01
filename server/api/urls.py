23a11a1a9de8c2b7b6279b4bf23a3057fe2ee7db
Allen K Abraham <allenabraham777@gmail.com>
2021-05-01 17:28:01 +0530

    Bug fix in csv export

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('equities', views.fetchAll, name='FetchAllEquities'),
-  path('equities/export', views.exportCSV, name='ExportAsCsv'),
+  path('equities/export/', views.exportCSV, name='ExportAsCsv'),
   path('equities/<str:name>', views.fetchByName, name='FetchEquitiesByName'),
   path('equities/export/<str:name>', views.exportCSV, name='ExportAllAsCSV'),
   path('clean', views.flushDB, name='CleanAllEquities'),
