37dd2a8a37b67504509ca7e56fd0311045444780
Allen K Abraham <allenabraham777@gmail.com>
2021-05-01 15:38:16 +0530

    Adding app into settings.py

--- server/zerodha_bhavcopy_tracker/settings.py
+++ server/zerodha_bhavcopy_tracker/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab'
+    'django_crontab',
+    'zerodha_bhavcopy_tracker'
 ]
 
 MIDDLEWARE = [
