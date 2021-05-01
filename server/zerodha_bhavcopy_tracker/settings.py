5cfcbf8c55b4220adc6c70adefb8bde233aee0d2
Allen K Abraham <allenabraham777@gmail.com>
2021-05-01 16:01:29 +0530

    Few tweaks in settings.py

--- server/zerodha_bhavcopy_tracker/settings.py
+++ server/zerodha_bhavcopy_tracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
